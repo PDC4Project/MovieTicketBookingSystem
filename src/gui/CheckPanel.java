6dbcf75085f1ce4bcbafe8def9f2bd96727182bd
xx-111 <871017353@qq.com>
2021-01-16 10:46:58 +0800

    no message

--- src/gui/CheckPanel.java
+++ src/gui/CheckPanel.java
@@ -12,7 +12,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField; 
 class SwingLoginExample {
     
