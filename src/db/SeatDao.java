d3469a9d72b9f2849e5f6e8961382e22eb39cc72
K <906879240@qq.com>
2021-01-16 09:48:28 +0800

    no message

--- src/db/SeatDao.java
+++ src/db/SeatDao.java
@@ -1,9 +1,20 @@
 package db;
 
+import java.sql.Connection;
+
 /**
  *
  * @author zk
  */
 public class SeatDao {
-    
+     private final DB db;
+    private final Connection conn;
+    public SeatDao(){
+        db = new DB();
+        conn = db.getConnection();
+    }
+    public void select(int roomId)
+    {
+        
+    }
 }
