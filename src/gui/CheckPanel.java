300f5d0d70c9d917fe171fa89f1fddc4955501a0
xx-111 <871017353@qq.com>
2021-01-20 09:15:07 +0800

    no message

--- src/gui/CheckPanel.java
+++ src/gui/CheckPanel.java
@@ -8,32 +8,29 @@ package gui;
  *
  * @author DELL
  */
+import db.CustomerDao;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField; 
+import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
-public class CheckPanel {
+public class CheckPanel extends JFrame{
     
-    public static void main(String[] args) {    
-
-        JFrame frame = new JFrame("订单界面");
-        frame.setSize(600, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel();    
-        frame.add(panel);
-        placeComponents(panel);
-        frame.setVisible(true);
-        
-        
-        
-        
-    }
+  
 
     private static void placeComponents(JPanel panel) {
 
@@ -44,40 +41,62 @@ public class CheckPanel {
         userLabel.setBounds(280,20,80,25);
         panel.add(userLabel);
 
-        
-
         JLabel Label_11 = new JLabel("电影名称：");
         Label_11.setBounds(10,50,80,25);
         panel.add(Label_11);
- 
         
         JLabel Label_21 = new JLabel("放映时间：");
         Label_21.setBounds(140,50,80,25);
         panel.add(Label_21);
-
-        
-         JLabel Label_31 = new JLabel("放映厅号：");
+       
+        JLabel Label_31 = new JLabel("放映厅号：");
         Label_31.setBounds(270,50,80,25);
         panel.add(Label_31); 
         
- 
         JLabel Label_41 = new JLabel("您的座位：");
         Label_41.setBounds(400,50,80,25);
-        panel.add(Label_41);
-        
-        
-//        JTextField Text = new JTextField(20);
-//        Text.setBounds(10,80,500,400);
-//        panel.add(Text);
-        JTable table = new JTable(10, 4);
-        table.setBounds(10,80,500,160);
-        panel.add(table);
+        panel.add(Label_41);     
+//        JTable table = new JTable(10, 4);
+//        table.setBounds(10,80,500,160);
+//        panel.add(table);
+        JButton r = new JButton("退票");
+        r.setBounds(10, 500, 80, 25);
+        panel.add(r);
 
-        JButton loginButton = new JButton("退票");
-        loginButton.setBounds(10, 500, 80, 25);
-        panel.add(loginButton);
-
-
+        r.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new RefundPanel().setVisible(true);
+            }
+        });  
     }
+    
+    public JPanel addMessage() {
+        JPanel m = new JPanel();
+        m.setLayout(new GridLayout(1,4));
+        m.setBorder(BorderFactory.createTitledBorder("电影订单"));
 
+        JLabel l1 = new JLabel("name");
+        JLabel l2 = new JLabel("time");
+        JLabel l3 = new JLabel("room");
+        JLabel l4 = new JLabel("seat");
+        m.add(l1);
+        m.add(l2);
+        m.add(l3);
+        m.add(l4);
+        return m;
+    } 
+    
+
+     public static void main(String[] args) {    
+
+        JFrame frame = new JFrame("订单界面");
+        frame.setSize(600, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();    
+        frame.add(panel);
+        placeComponents(panel);
+        frame.setVisible(true);
+               
+    }
 }
--- src/gui/RefundPanel.java
+++ src/gui/RefundPanel.java
@@ -16,7 +16,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField; 
-class RefundPanel {
+class RefundPanel extends JFrame{
     
     public static void main(String[] args) {    
 
